d84b8cad535995668e14c1cfbde17a4ba7c3decb
Eduardo Liriano <Lirianobaeze@gmail.com>
2024-06-21 17:49:45 -0400

    Una vez mas por que no aviamos agregado los archivos con (git add.) 😂

---
+++ Views/DATA_VIEWS/EventoView.xaml.cs
@@ -0,0 +1,42 @@
+using Microsoft.Maui.Controls;
+using System.Collections.Generic;
+using TapPay.Models;
+using TapPay.Services;
+
+namespace TapPay.Views
+{
+    public partial class EventoView : ContentPage
+    {
+        private DatabaseService _databaseService;
+        private int usuario_id;
+        public EventoView(int usuario_id)
+        {
+            InitializeComponent();
+            string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "TapPay.db3");
+            string sqlServerConnectionString = "Server=192.168.68.103,1433;Database=TapPay;User Id=sa;Password=5a$Rv9&d2!Fm;TrustServerCertificate=True";
+            LoadData();
+            _databaseService = new DatabaseService(dbPath, sqlServerConnectionString);
+
+            this.usuario_id = usuario_id;
+            
+        }
+
+
+         private async void OnAgregarEventoClicked(object sender, EventArgs e)
+        {
+        
+             await Navigation.PushAsync(new ADD_Evento(usuario_id));
+        }
+
+        private async void LoadData()
+        {
+            // Cargar los datos de la base de datos
+            List<Evento> eventos = await App.Database.GetEventosAsync(usuario_id);
+
+
+            // Vincular los datos a los CollectionView
+            EventosCollectionView.ItemsSource = eventos;
+            // Agrega más llamadas a métodos de carga de datos y vinculación para Usuarios, Tarjetas NFC, Productos, Transacciones según sea necesario
+        }
+    }
+}
